5bdd375d1dcd208f1caed23684436faeee58e3a3
daniel dantas <contatodanielsenac@gmail.com>
2021-12-27 17:35:08 -0300

    Finalizando a sessão dos projetos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* Dark Mode */
 let menu = document.querySelector('#menu-btn')
 let navbar = document.querySelector('.l-header .l-nav')
 
@@ -25,3 +26,31 @@ menuHamburguer.addEventListener('click', () => {
         .querySelector('.l-nav__container')
         .classList.toggle('show-menu')
 })
+
+/* Orçamento */
+document.querySelector('#qtde').addEventListener('change', atualizarPreco)
+document.querySelector('#js').addEventListener('change', atualizarPreco)
+document.querySelector('#layout-sim').addEventListener('change', atualizarPreco)
+document.querySelector('#layout-nao').addEventListener('change', atualizarPreco)
+document.querySelector('#prazo').addEventListener('change', function () {
+    const prazo = document.querySelector('#prazo').value
+    document.querySelector(
+        'label[for=prazo]'
+    ).innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+
+function atualizarPreco() {
+    const qtde = document.querySelector('#qtde').value
+    const temJS = document.querySelector('#js').checked
+    const incluiLayout = document.querySelector('#layout-sim').checked
+    const prazo = document.querySelector('#prazo').value
+
+    let preco = qtde * 100
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo * 0.1
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector('#preco').innerHTML = `R$ ${preco.toFixed(2)}`
+}
